84a7fd49a674774eed91d936a494c56702695845
Samir Noir <samir@noir.im>
2026-02-14 19:16:10 +0100

    feat: add jail_create.sh script, to boostrap jails with pkgbase

---
+++ jail_create.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+# Create a jail using pkgbase, only FreeBSD 15 is supported
+#
+
+set -eu
+
+create_zfs_dataset() {
+	if ! zfs create "$1"; then
+		return 1
+	else
+		echo "ZFS dataset $1 created, mountpoint: $(zfs get -H mountpoint "$1" | awk '{ print $3}')"
+		return 0
+	fi
+}
+
+zfs_dataset_prefix="${ZFS_DATASET_PREFIX:-zroot/data/jails}"
+
+if [ $# -lt 1 ]; then
+	echo "Please provide a jail name in argument" >&2
+	exit 1
+fi
+
+jail_name=$1
+zfs_dataset_base=${zfs_dataset_prefix}/${jail_name}
+zfs_dataset_root=${zfs_dataset_prefix}/${jail_name}/root
+
+create_zfs_dataset "$zfs_dataset_base" || exit 1
+create_zfs_dataset "$zfs_dataset_root" || exit 1
+
+zfs_dataset_root_mountpoint=$(zfs get -H mountpoint "$zfs_dataset_root" | awk '{ print $3}')
+
+mkdir -p "$zfs_dataset_root_mountpoint/usr/share/keys/pkgbase-15/"
+cp -r /usr/share/keys/pkgbase-15/* "$zfs_dataset_root_mountpoint/usr/share/keys/pkgbase-15/"
+pkg --rootdir "$zfs_dataset_root_mountpoint" install -y FreeBSD-set-minimal-jail
+mkdir -p "$zfs_dataset_root_mountpoint/usr/local/etc/pkg/repos/"
+cat > "$zfs_dataset_root_mountpoint/usr/local/etc/pkg/repos/FreeBSD-base.conf"<<EOF
+FreeBSD-base: {
+  enabled: yes
+}
+EOF
